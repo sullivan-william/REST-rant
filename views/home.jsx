e251b2474d85fbc000b92247aa4d835ebc47c1b1
William Sullivan <sullivanwp95@gmail.com>
2022-09-29 21:14:51 -0400

    adding image to home page

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src='/images/tacos.jpg' alt='tacos' style={{width: '30em', height: '30em'}}/>
+                    <div>
+                        Photo by <a href="https://unsplash.com/@briewilly?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Chad Montano</a> on <a href="https://unsplash.com/s/photos/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
+                </div>
                 <a href='/places'>
                     <button className="btn-primary">Places Page</button>
                 </a>
