f541d9c8228a260f64333878a4653dedae81aef8
William Sullivan <sullivanwp95@gmail.com>
2022-10-03 12:08:36 -0400

    Created seeder file for mock data

---
+++ seeders/seed-places.js
@@ -0,0 +1,26 @@
+const db = require('../models')
+
+db.Place.create([{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/thai.jpg',
+    founded: 1989
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/cat-cafe.jpg',
+    founded: 2020
+}])
+
+.then(() => {
+    console.log('success')
+    process.exit()
+})
+.catch((err) => {
+    console.log('failure', err)
+    process.exit()
+})
