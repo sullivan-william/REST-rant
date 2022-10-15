d83af2380408ae67dec9fc343daf2ba781dee5ad
William Sullivan <sullivanwp95@gmail.com>
2022-10-15 18:27:58 -0400

    Displaying comments on show page

--- controllers/places.js
+++ controllers/places.js
@@ -43,6 +43,7 @@ router.get('/new', (req, res) => {
 // GET route for specific place's show page
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
+  .populate('comments')
   .then((place) => {
     res.render('places/show', { place })
   })
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,28 @@ const React = require('react')
 const Def = require('../default')
 
 function show(data) {
+
+    let comments = (
+        <h3 className='inactive'>
+            No comments yet!
+        </h3>
+    )
+
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div className='border'>
+                    <h2 className='rant'>{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <strong>- {c.author}</strong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
+
     return (
         <Def>
             <main>
@@ -21,6 +43,7 @@ function show(data) {
                 </div>
                 <div>
                     <h2>Comments</h2>
+                    {comments}
                 </div>
                 <div>
                     <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
