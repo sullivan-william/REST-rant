4576a9c158154da649147174e51f5668062d0d6d
William Sullivan <sullivanwp95@gmail.com>
2023-12-11 20:21:15 -0500

    Fix home page styling for mobile devices

--- views/home.jsx
+++ views/home.jsx
@@ -1,19 +1,20 @@
 const React = require('react')
 const Def = require('./default')
 
-function home () {
+function home() {
     return (
         <Def>
             <main>
-                <h1>REST-rant</h1>
+                <h1 id='home-h1'>REST-<br></br>rant</h1>
+                <br></br>
                 <div>
-                    <img src='/images/tacos.jpg' alt='tacos' style={{width: '30em', height: '30em'}}/>
+                    <img src='/images/tacos.jpg' alt='tacos' style={{ width: '80vw', height: 'auto' }} />
                     <div>
                         Photo by <a href="https://unsplash.com/@briewilly?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Chad Montano</a> on <a href="https://unsplash.com/s/photos/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
                     </div>
                 </div>
                 <br></br>
-                <a href='/places'>
+                <a href='/places' className='large'>
                     <button className="teal-btn">Places Page</button>
                 </a>
             </main>
